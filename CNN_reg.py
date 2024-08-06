a4bc35ab951708d973374383d62519efc267f463
Zhang-Koda <153485315+Zhang-Koda@users.noreply.github.com>
2024-08-06 10:04:41 +0800

    CNN_reg.py

--- CNN_reg.py
+++ CNN_reg.py
@@ -13,14 +13,7 @@ import torch
 from torch import nn
 from torch.utils.data import TensorDataset
 from torch.utils.data import DataLoader
-from datetime import date
 from d2l import torch as d2l
-from sklearn.model_selection import train_test_split
-import math
-from datetime import timedelta
-
-
-
 
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -49,17 +42,11 @@ def MSE_cal(Y_hat, y):
     '''
     temp_data = torch.square(Y_hat - y)
     total = temp_data.numel()
-    
     # Replace NaN values with zeros
     temp_data = torch.nan_to_num(temp_data, nan=0.0)
     mse = temp_data.sum() / total
     
     return mse
-
-
-
-
-
 
 
 
@@ -78,22 +65,14 @@ def standardization(input_data,comp_data):
     if input_data.size == 0 or comp_data.size == 0:
         raise ValueError("Input arrays must not be empty.")
         
-    comp_data_flat = comp_data.flatten()
-    comp_data_flat = comp_data_flat[~np.isnan(comp_data_flat)]
-
-    mean_value = np.mean(comp_data_flat)
-    std_deviation = np.std(comp_data_flat)
+    mean_value = np.nanmean(comp_data_flat)
+    std_deviation = np.nanstd(comp_data_flat)
 
     if std_deviation == 0:
         raise ValueError("Standard deviation is zero. Cannot perform standardization.")
 
     standardized_data = (input_data - mean_value) / std_deviation
     return standardized_data
-
-
-
-
-
 
 
 
@@ -128,14 +107,9 @@ def evaluation(net, data_iter, device=None):
             total_mse += MSE_cal(y_hat, y)
             
     average_mse = total_mse / num_batches
-    
-    
+
     return average_mse
            
-
-
-
-
 
 
 
@@ -157,7 +131,6 @@ def data_generation(net, data_iter, lat, lon, filename):
     if isinstance(net, nn.Module):
         net.eval() 
     
-    
     data = np.empty((0,lat,lon))
     
     with torch.no_grad():
@@ -168,7 +141,6 @@ def data_generation(net, data_iter, lat, lon, filename):
             y_hat = y_hat.cpu().numpy()
             data = np.append(data, y_hat,axis=0)
 
-    
     scio.savemat(filename ,{'data':data}) 
 
 
@@ -184,7 +156,7 @@ def init_weights(m):
         nn.init.xavier_uniform_(m.weight)  #variance to remain the same
         
 def train_CNN_model(net, train_iter, tst_iter1, tst_iter2, num_epochs, 
-               num_lat, num_lon, depth, dataset_trn, dataset_tst1, dataset_tst2):    
+               num_lat, num_lon, dataset_trn, dataset_tst1, dataset_tst2):    
     '''
     Train a Convolutional Neural Network (CNN) model.
     
@@ -196,7 +168,6 @@ def train_CNN_model(net, train_iter, tst_iter1, tst_iter2, num_epochs,
     - num_epochs : Number of training epochs.
     - num_lat : Number of latitude points.
     - num_lon : Number of longitude points.
-    - depth : The depth.
     - dataset_trn : The training dataset.
     - dataset_tst1 : The 1st test dataset.
     - dataset_tst2 : The 2nd test dataset.
@@ -212,8 +183,7 @@ def train_CNN_model(net, train_iter, tst_iter1, tst_iter2, num_epochs,
     net.apply(init_weights)
     print('training on', device)
     net.to(device)
-    
- 
+
     optimizer = torch.optim.Adadelta(net.parameters())
     
     loss = nn.MSELoss()
@@ -224,15 +194,10 @@ def train_CNN_model(net, train_iter, tst_iter1, tst_iter2, num_epochs,
     Tst_mse1 = np.empty(0)
     Tst_mse2 = np.empty(0)
     for epoch in range(num_epochs):
-        
-        print('epoch1:')
-        print(epoch)
-   
+        print('epoch:'+str(epoch))
+
         net.train()
-      
-        L=len(train_iter)
         trn_mse = 0
-    
         for i, (X, y) in enumerate(train_iter):  
         
             timer.start()
@@ -251,7 +216,7 @@ def train_CNN_model(net, train_iter, tst_iter1, tst_iter2, num_epochs,
             timer.stop()
             trn_mse += mse
             
-        trn_mse = trn_mse / L
+        trn_mse = trn_mse / len(train_iter)
         trn_mse = trn_mse.cpu().numpy()
         Trn_mse = np.append(Trn_mse, trn_mse)
         
@@ -270,23 +235,11 @@ def train_CNN_model(net, train_iter, tst_iter1, tst_iter2, num_epochs,
               f'test1 mse {tst_mse1:.3f},'
               f'test2 mse {tst_mse2:.3f},')
 
-        
-            
-    
-    # for the 1st test dataset 
-    savepath = './'+dataset_trn+'-'+dataset_tst1+'/'
-    filename = savepath + 'Y_'+str(depth)+'.mat'
-    data_generation(net, tst_iter1, num_lat, num_lon, filename)  
-    
-    # for the 2nd test dataset 
-    savepath = './'+dataset_trn+'-'+dataset_tst2+'/'
-    filename = savepath +  'Y_'+str(depth)+'.mat'
-    data_generation(net, tst_iter2, num_lat, num_lon, filename)  
-    
-    
-    
-    
     return  Trn_mse ,Tst_mse1, Tst_mse2
+
+
+
+
 
 
 """
@@ -296,7 +249,7 @@ def train_CNN_model(net, train_iter, tst_iter1, tst_iter2, num_epochs,
 """
 
 depth = 25
-Batch_size = 365
+Batch_size = 512
 dataset_trn = 'ECCO2'
 dataset_tst1 = 'GLORYS'
 dataset_tst2 = 'HYCOM'
@@ -304,43 +257,11 @@ dataset_tst2 = 'HYCOM'
 
 
 LON = np.arange(0.125, 360.125, 1)  
-LAT = np.arange(-59.375, 60.625, 1)
-
-    
+LAT = np.arange(-57.375, 58.625, 1)
 
 
 num_lon = len(LON)
 num_lat = len(LAT)
-
-# X & Y coordinates for training and test datasets
-XX_1  =  np.arange(0.125, 359.875, 0.5)
-YY_1  =  np.arange(-89.875, 89.875, 0.5)
-XX_2  =  np.arange(0.125, 359.875, 0.5)
-YY_2  =  np.arange(-79.875, 89.875, 0.5)
-XX_3  =  np.arange(0.125, 359.875, 0.5)
-YY_3  =  np.arange(-59.875, 59.875, 0.5)
-
-
-# time for training and test datasets
-year_from1 = 1993
-year_trn_begin  =  1994
-year_trn_end    =  2019
-
-year_from2 = 1992
-year_tst1_begin  =  1994
-year_tst1_end    =  2019
-
-year_from3 = 1993
-year_tst2_begin  =  1994
-year_tst2_end    =  2011
-
-time_trn = np.arange(date.toordinal(date(year_trn_begin, 1, 1)), date.toordinal(date(year_trn_end, 12, 31) + timedelta(days=1))) - date.toordinal(date(year_from1, 1, 1))
-time_tst1 = np.arange(date.toordinal(date(year_tst1_begin, 1, 1)), date.toordinal(date(year_tst1_end, 12, 31) + timedelta(days=1))) - date.toordinal(date(year_from2, 1, 1))
-time_tst2 = np.arange(date.toordinal(date(year_tst2_begin, 1, 1)), date.toordinal(date(year_tst2_end, 12, 31) + timedelta(days=1))) - date.toordinal(date(year_from3, 1, 1))
-
-time_trn = time_trn.astype(int)
-time_tst1 = time_tst1.astype(int)
-time_tst2 = time_tst2.astype(int)
 
 
 
@@ -351,46 +272,20 @@ time_tst2 = time_tst2.astype(int)
 """
 
 # =================================training dataset============================
+# Sea surface height anomaly
+filename='./'+dataset_trn+'/SSHA/'+'ssha.mat'
+data=h5py.File(filename,mode='r')
+SSHA = data['ssha'][:]
 
-SSHA    =   np.full([len(time_trn),num_lat, num_lon], np.nan)
-SSTA    =   np.full([len(time_trn),num_lat, num_lon], np.nan)
-SUBTA     =   np.full([len(time_trn),num_lat, num_lon], np.nan)
-
-
-M1    =  np.where(YY_1==LAT[0])[0]
-M2    =  np.where(YY_1==LAT[-1])[0]
-N1    =  np.where(XX_1==LON[0])[0]
-N2    =  np.where(XX_1==LON[-1])[0]
-
-
-print('load training dataset')
-for i in range(6): 
+# Sea surface temperature anomaly
+filename='./'+dataset_trn+'/SSTA/'+'ssta.mat'
+data=h5py.File(filename,mode='r')
+SSTA = data['ssta'][:]
     
-    part = i + 1
-    
-    # Sea surface height anomaly
-    filename='./'+dataset_trn+'/SSHA/'+'ssha'+str(part)+'.mat'
-    data=h5py.File(filename,mode='r')
-    data = data['ssha'][:]
-    data= data.transpose(0,2,1)
-    SSHA[:,:,i*60:(i+1)*60]=data[time_trn,M1[0]:M2[0]+1:2,::2]
-    
-    # Sea surface temperature anomaly
-    filename='./'+dataset_trn+'/SSTA/'+'ssta'+str(part)+'.mat'
-    data=h5py.File(filename,mode='r')
-    data = data['ssta'][:]
-    data= data.transpose(0,2,1)
-    SSTA[:,:,i*60:(i+1)*60]=data[time_trn,M1[0]:M2[0]+1:2,::2]
-    
-    
-    # Subsurface temperature anomaly
-    filename='./'+dataset_trn+'/'+str(depth)+'m/'+'SUBTA'+str(part)+'.mat'
-    data=h5py.File(filename,mode='r')
-    data = data['subta'][:]
-    data= data.transpose(0,2,1)
-    SUBTA[:,:,i*60:(i+1)*60]=data[time_trn,M1[0]:M2[0]+1:2,::2]
-
-
+# Subsurface temperature anomaly
+filename='./'+dataset_trn+'/'+str(depth)+'m/'+'SUBTA.mat'
+data=h5py.File(filename,mode='r')
+SUBTA = data['subta'][:]
 
 
 # Normalization
@@ -406,7 +301,7 @@ YY_TRN = torch.tensor(SUBTA, dtype=torch.float32)
 
 # API
 train_ids = TensorDataset(XX_TRN, YY_TRN) 
-train_iter_False = DataLoader(dataset=train_ids, batch_size=Batch_size, shuffle=False)  #For calculating training error.
+train_iter_False = DataLoader(dataset=train_ids, batch_size=Batch_size, shuffle=False)  
 trn_iter_shuffle = DataLoader(dataset=train_ids, batch_size=Batch_size, shuffle=True)
 
 
@@ -414,44 +309,20 @@ trn_iter_shuffle = DataLoader(dataset=train_ids, batch_size=Batch_size, shuffle=
 
 
 # ================================= tst1 dataset ==============================
+# Sea surface height anomaly
+filename='./'+dataset_tst1+'/SSHA/'+'ssha.mat'
+data=h5py.File(filename,mode='r')
+SSHA1 = data['ssha'][:]
 
-SSHA1    =   np.full([len(time_tst1),num_lat, num_lon], np.nan)
-SSTA1    =   np.full([len(time_tst1),num_lat, num_lon], np.nan)
-SUBTA1   =   np.full([len(time_tst1),num_lat, num_lon], np.nan)
+# Sea surface temperature anomaly
+filename='./'+dataset_tst1+'/SSTA/'+'ssta.mat'
+data=h5py.File(filename,mode='r')
+SSTA1 = data['ssta'][:]
 
-M1    =  np.where(YY_2==LAT[0])[0]
-M2    =  np.where(YY_2==LAT[-1])[0]
-N1    =  np.where(XX_2==LON[0])[0]
-N2    =  np.where(XX_2==LON[-1])[0]
-
-
-print('load tst1 dataset')
-for i in range(6): 
-    
-    part = i + 1
-    
-    # Sea surface height anomaly
-    filename='./'+dataset_tst1+'/SSHA/'+'ssha'+str(part)+'.mat'
-    data=h5py.File(filename,mode='r')
-    data = data['ssha'][:]
-    data= data.transpose(0,2,1)
-    SSHA1[:,:,i*60:(i+1)*60]=data[time_tst1,M1[0]:M2[0]+1:2,::2]
-
-    # Sea surface temperature anomaly
-    filename='./'+dataset_tst1+'/SSTA/'+'ssta'+str(part)+'.mat'
-    data=h5py.File(filename,mode='r')
-    data = data['ssta'][:]
-    data= data.transpose(0,2,1)
-    SSTA1[:,:,i*60:(i+1)*60]=data[time_tst1,M1[0]:M2[0]+1:2,::2]
-    
-    # Subsurface temperature anomaly
-    filename='./'+dataset_tst1+'/'+str(depth)+'m/'+'SUBTA'+str(part)+'.mat'
-    data=h5py.File(filename,mode='r')
-    data = data['subta'][:]
-    data= data.transpose(0,2,1)
-    SUBTA1[:,:,i*60:(i+1)*60]=data[time_tst1,M1[0]:M2[0]+1:2,::2]
-
-
+# Subsurface temperature anomaly
+filename='./'+dataset_tst1+'/'+str(depth)+'m/'+'SUBTA.mat'
+data=h5py.File(filename,mode='r')
+SUBTA1 = data['subta'][:]
 
 
 SSHA1 = standardization(SSHA1,SSHA1)
@@ -475,46 +346,20 @@ tst_iter1 = DataLoader(dataset=tst_ids1, batch_size=Batch_size, shuffle=False)
 
 
 # ================================= tst2 dataset ==============================
+# Sea surface height anomaly
+filename='./'+dataset_tst2+'/SSHA/'+'ssha.mat'
+data=h5py.File(filename,mode='r')
+SSHA2 = data['ssha'][:]
 
-SSHA2    =   np.full([len(time_tst2),num_lat, num_lon], np.nan)
-SSTA2    =   np.full([len(time_tst2),num_lat, num_lon], np.nan)
-SUBTA2   =   np.full([len(time_tst2),num_lat, num_lon], np.nan)
+# Sea surface temperature anomaly
+filename='./'+dataset_tst2+'/SSTA/'+'ssta.mat'
+data=h5py.File(filename,mode='r')
+SSTA2 = data['ssta'][:]
 
-M1    =  np.where(YY_3==LAT[0])[0]
-M2    =  np.where(YY_3==LAT[-1])[0]
-N1    =  np.where(XX_3==LON[0])[0]
-N2    =  np.where(XX_3==LON[-1])[0]
-
-
-print('load tst2 dataset')
-for i in range(6): 
-    
-    part = i + 1
-    
-    # Sea surface height anomaly
-    filename='./'+dataset_tst2+'/SSHA/'+'ssha'+str(part)+'.mat'
-    data=h5py.File(filename,mode='r')
-    data = data['ssha'][:]
-    data= data.transpose(0,2,1)
-    SSHA2[:,:,i*60:(i+1)*60]=data[time_tst2,M1[0]:M2[0]+1:2,::2]
-
-
-    # Sea surface temperature anomaly
-    filename='./'+dataset_tst2+'/SSTA/'+'ssta'+str(part)+'.mat'
-    data=h5py.File(filename,mode='r')
-    data = data['ssta'][:]
-    data= data.transpose(0,2,1)
-    SSTA2[:,:,i*60:(i+1)*60]=data[time_tst2,M1[0]:M2[0]+1:2,::2]
-    
-    
-    # Subsurface temperature anomaly
-    filename='./'+dataset_tst2+'/'+str(depth)+'m/'+'SUBTA'+str(part)+'.mat'
-    data=h5py.File(filename,mode='r')
-    data = data['subta'][:]
-    data= data.transpose(0,2,1)
-    SUBTA2[:,:,i*60:(i+1)*60]=data[time_tst2,M1[0]:M2[0]+1:2,::2]
-
-
+# Subsurface temperature anomaly
+filename='./'+dataset_tst2+'/'+str(depth)+'m/'+'SUBTA.mat'
+data=h5py.File(filename,mode='r')
+SUBTA2 = data['subta'][:]
 
 
 SSHA2 = standardization(SSHA2,SSHA2)
@@ -562,21 +407,24 @@ for layer in net:
 
 
 
- 
-
-
-
-  
   
 num_epochs = 100
 
 
 Trn_mse, Tst_mse1, Tst_mse2 = train_CNN_model(net, trn_iter_shuffle,tst_iter1, tst_iter2,
-                                           num_epochs, num_lat,num_lon, depth,dataset_trn, dataset_tst1, dataset_tst2)
+                                           num_epochs, num_lat,num_lon, dataset_trn, dataset_tst1, dataset_tst2)
 
 
+ # for the 1st test dataset 
+savepath = './'+dataset_trn+'-'+dataset_tst1+'/'
+filename = savepath + 'Y_'+str(depth)+'.mat'
+data_generation(net, tst_iter1, num_lat, num_lon, filename)  
 
-
+# for the 2nd test dataset 
+savepath = './'+dataset_trn+'-'+dataset_tst2+'/'
+filename = savepath +  'Y_'+str(depth)+'.mat'
+data_generation(net, tst_iter2, num_lat, num_lon, filename)  
+    
 print('--------------------------------------------------------')
 
 savepath1 = './'+dataset_trn+'-'+dataset_trn+'/'
@@ -587,7 +435,5 @@ savepath3 = './'+dataset_trn+'-'+dataset_tst2+'/'
 scio.savemat(savepath3+'Tst_mse2'+str(depth)+'.mat',{'Tst_mse2':Tst_mse2})
 
 
-
-    
 torch.save(net.state_dict(), 'sequential_model_'+str(depth)+'.pth')
 
